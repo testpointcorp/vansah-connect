ef47d1796c4b1c04636b633224ce8f06c285b8a4
shubham mourya <=>
2023-09-21 00:11:38 +0530

    Added Unit Test Cases

--- cli.test.js
+++ cli.test.js
@@ -1,14 +1,14 @@
 import { expect } from "chai";
 import { sendResult,sendTestCaseResult } from "./api/sendresults.js"; 
 import { getConnectToken } from "./utility/setGetToken.js";
+import { result , testCaseResult} from "./utility/validation.js";
 
-describe("API Tests", () => {
+describe("API Tests", function() {
     
   it("API should be able to upload the testngResults.xml to Vansah", function(done){
     getConnectToken();
     sendResult("./testng-report.xml", process.env.TOKEN)
       .then(function(result) {
-        console.log(result.data.message);
         expect(result.data.message).to.equal("Results import is completed.");
         done();  
       })
@@ -20,7 +20,6 @@ describe("API Tests", () => {
     getConnectToken();
     sendTestCaseResult("PVT-C500","2","PVT-4",process.env.TOKEN)
       .then(function(result) {
-        console.log(result.data.message);
         expect(result.data.message).to.equal("A new Test Run created.");
         done();  
       })
@@ -29,4 +28,25 @@ describe("API Tests", () => {
       });
   });
 
+});
+describe("Validate validation.js functionality",function(){
+  it("Validate result ()", function(done){
+  result("./testng-report.xml")
+      .then(function() {
+        done();  
+      })
+      .catch(function(err) {
+        done(err);  
+      });
+  });
+  it("Validate testCaseResult()", function(done){
+    testCaseResult("PVT-C500","2","PVT-4")
+        .then(function() {
+          done();  
+        })
+        .catch(function(err) {
+          done(err);  
+        });
+    });
+  
 });
